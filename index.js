da42b487529ea2788db9781a82c19d7683707acb
Sanjeewa Samudra <sanjeewa@cultdigital.com>
2024-12-08 23:48:24 +0200

    inital commit glamorous

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ app.use('/admin', adminRoutes);
 app.use("/", homeRoutes);
 
 connectDB();
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
 
+// Vercel expects app to be exported, not running on a listening port.
 module.exports = app;
