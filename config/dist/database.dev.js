d07df5234b0d0d5ee3161115698d7317977a8cb1
Sanjeewa Samudra <sanjeewa@cultdigital.com>
2024-12-10 00:01:03 +0200

    Initial Commit

---
+++ config/dist/database.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var mongoose = require("mongoose");
+
+var connectDB = function connectDB() {
+  return regeneratorRuntime.async(function connectDB$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(mongoose.connect(process.env.MONGODB_URI, {//useNewUrlParser: true,
+            //useUnifiedTopology: true,
+          }));
+
+        case 3:
+          console.log("MongoDB Connected");
+          _context.next = 10;
+          break;
+
+        case 6:
+          _context.prev = 6;
+          _context.t0 = _context["catch"](0);
+          console.error(_context.t0.message);
+          process.exit(1);
+
+        case 10:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 6]]);
+};
+
+module.exports = connectDB;
